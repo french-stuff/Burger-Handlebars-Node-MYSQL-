2c4087f3064c43fe01f783a4942d21b0c3c97c8b
French Stuff <legarreta.french@gmail.com>
2020-02-22 01:10:45 -0500

    added burgers to table

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom and Swiss Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Vegetarian Garden Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Impossible Burger", false);
