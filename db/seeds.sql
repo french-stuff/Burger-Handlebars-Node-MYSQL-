21d9f696e25b323fbe682ed15e14483ba35d584f
french-stuff <53927605+french-stuff@users.noreply.github.com>
2020-03-07 17:51:44 -0500

    seeds.sql

--- db/seeds.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom and Swiss Burger", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Vegetarian Garden Burger", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Impossible Burger", false);
