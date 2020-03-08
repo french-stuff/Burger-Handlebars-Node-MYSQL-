f0ad8f97122e0566711dc21993d8837cf62cdc5f
French Stuff <legarreta.french@gmail.com>
2020-03-07 23:01:36 -0500

    added port listening

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const handlebars = require("express-handlebars");
 const mysql = require("mysql");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
