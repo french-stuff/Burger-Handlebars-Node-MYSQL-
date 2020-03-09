3402b8b1180d8bced4333078826e424eec128bdd
French Stuff <legarreta.french@gmail.com>
2020-03-09 10:38:10 -0400

    update

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.engine("handlebars", exphbs({ defaultLayout "main"}));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgers_controller.js");
+var routes = require(".controller/burgers_controller");
 app.use(routes);
 
 
